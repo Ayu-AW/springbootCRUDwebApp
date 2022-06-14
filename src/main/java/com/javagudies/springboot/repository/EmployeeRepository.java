30c7f48cb8432ecbdf9c3cd692ad14064a07b2b4
Ayalew Wolde <ayalewawolde@gmail.com>
2022-06-14 11:56:54 -0700

    firs commit

---
+++ src/main/java/com/javagudies/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.javagudies.springboot.repository;
+
+import com.javagudies.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
---
+++ src/main/java/com/javagudies/springboot/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.javagudies.springboot.service;
+
+import com.javagudies.springboot.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> getAllEmployees();
+    void saveEmployee(Employee employee);
+    Employee getEmployeeById(long id);
+    void deleteEmployeeById(long id);
+}
